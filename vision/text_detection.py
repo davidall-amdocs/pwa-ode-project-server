24b51f217aca806835e62a8e9097c098e31b838c
David Lopez <davidlopeztorres03@gmail.com>
2021-07-12 20:54:51 -0500

    Remove temporal file implementation

--- vision/text_detection.py
+++ vision/text_detection.py
@@ -9,13 +9,10 @@ client = vision.ImageAnnotatorClient()
 
 def detectText(imgText):
     imgTextEnc = imgText.encode('utf-8')
-    with open('decoded_image.png', 'wb') as file_to_save:
-        decoded_image_data = base64.decodebytes(imgTextEnc)
-        image = vision.Image(content = decoded_image_data)
-        textDetected = client.document_text_detection(image = image)
-        finalText = textDetected.full_text_annotation.text.replace("\n", "")
-        finalText = finalText.replace(" ", "")
-        return finalText
-
-
+    decoded_image_data = base64.decodebytes(imgTextEnc)
+    image = vision.Image(content = decoded_image_data)
+    textDetected = client.document_text_detection(image = image)
+    finalText = textDetected.full_text_annotation.text.replace("\n", "")
+    finalText = finalText.replace(" ", "")
+    return finalText
 
