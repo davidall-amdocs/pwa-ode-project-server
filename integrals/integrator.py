3bfaa28260c4dff9e16f537bc608218d8c817904
David Lopez <davidlopeztorres03@gmail.com>
2021-09-22 13:07:07 -0500

    Recursive and Trees implementation

--- integrals/integrator.py
+++ integrals/integrator.py
@@ -1,10 +1,11 @@
-from utils.constant import ATOMIC_INTEGRAL_DIFFICULTY
+from utils.constant import ATOMIC_INTEGRAL_DIFFICULTY, MAX_GLOBAL_DIFFICULTY, MAX_INTEGRAL_LEVEL, MAX_NODE_DIFFICULTY, SYMPY_INTEGRAL
 from sympy import *
+from algebraics.operations import *
 
 import solvers.controller as controller
 import integrals.atomic_integrals as atm
 
-def int_solve(expression, differential):
+def int_atm_solve(expression, differential):
     '''
     # Params
     * expression (symbolic expression)
@@ -15,109 +16,156 @@ def int_solve(expression, differential):
     Uses local and global difficulty control
     '''
 
-    global variable_n
-    global variable_a
-    global variable_b
-    global variable_c
+    # Variables of global scope in integrator module
+    global variables
+    variables = [
+        {"symbol": Symbol('n'), "value": None}, 
+        {"symbol": Symbol('a'), "value": None}, 
+        {"symbol": Symbol('b'), "value": None}, 
+        {"symbol": Symbol('c'), "value": None}
+    ]
 
-    variable_n = None
-    variable_a = None
-    variable_b = None
-    variable_c = None
-
+    # Create list of atm integrals using the appropriate differential
     atm.build_integrals(differential)
 
-    global local_difficulty
-    local_difficulty = 0
-
-    expression = factor(expression)
+    # Factor expression for atm integral checking
+    expression = alg_factor(expression)
     differential_expression = Mul(expression, Symbol('d'+str(differential)))
-    print(srepr(differential_expression))
 
+    # Iterate all along the list of atomic integrals and use the match function to 
+    # verify for a possible match
     index = 0
     for integral in atm.BASIC:
         if match_integral(differential_expression, integral):
-            local_difficulty = local_difficulty + ATOMIC_INTEGRAL_DIFFICULTY
-            controller.global_difficulty = controller.global_difficulty + local_difficulty
-
             int_solution = atm.SOLVE[index]
 
-            if "Symbol('n')" in srepr(atm.SOLVE[index]):
-                int_solution = int_solution.subs(Symbol('n'), variable_n)
+            # Replace variables in integral solution
+            for variable in variables:
+                if srepr(variable['symbol']) in srepr(atm.SOLVE[index]):
+                    int_solution = int_solution.subs(variable['symbol'], variable['value'])    
 
-            if "Symbol('a')" in srepr(atm.SOLVE[index]):
-                int_solution = int_solution.subs(Symbol('a'), variable_a)
+            # Return symbolic expression and difficulty
+            return {"symbol": int_solution, "difficulty": ATOMIC_INTEGRAL_DIFFICULTY}
 
-            if "Symbol('b')" in srepr(atm.SOLVE[index]):
-                int_solution = int_solution.subs(Symbol('b'), variable_b)
-
-            if "Symbol('c')" in srepr(atm.SOLVE[index]):
-                int_solution = int_solution.subs(Symbol('c'), variable_c)
-
-            return int_solution
         index = index + 1
 
-    return integrate(expression, differential)
+    # If there's no match, then retun None
+    return None
+
+def tree_solve(expression, differential, level):
+
+    # Check level limit. If is too deep, then use integrate from sympy
+    if level >= MAX_INTEGRAL_LEVEL:
+        try:
+            print("Expression: " + str(expression))
+            print("Level: " + str(level))
+            print("Difficulty: " + str(SYMPY_INTEGRAL))
+            print()
+            return {"symbol": integrate(expression, differential), "difficulty": SYMPY_INTEGRAL}
+        except:
+            raise CompletenessAnomaly([["", []]])
+
+    # Check if the expression is a number
+    if expression.is_number:
+        atomic_int = int_atm_solve(Integer(1), differential)
+        print("Expression: " + str(expression))
+        print("Level: " + str(level))
+        print("Difficulty: " + str(atomic_int["difficulty"]))
+        print()
+        return {"symbol": alg_mul(expression, atomic_int["symbol"]), 
+        "difficulty": atomic_int["difficulty"] }
+
+    # Check if the expression is atomic
+    atomic_int = int_atm_solve(expression, differential)
+    if atomic_int is not None:
+        print("Expression: " + str(expression))
+        print("Level: " + str(level))
+        print("Difficulty: " + str(atomic_int["difficulty"]))
+        print()
+        return atomic_int
+
+    # Check if the expression has the form k*f(x) with
+    # k a constant number. Try to solve the new integral
+    if type(expression) is Mul and expression.args[0].is_number:
+        try:
+            aux_int = tree_solve(expression.args[1], differential, level+1)
+            print("Expression: " + str(expression))
+            print("Level: " + str(level))
+            print("Difficulty: " + str(aux_int["difficulty"]))
+            print()
+            return { "symbol": alg_mul(expression.args[0], aux_int["symbol"]), 
+            "difficulty": aux_int["difficulty"] }
+        
+        except CompletenessAnomaly as ca:
+            raise ca
+    
+    node_difficulty = 0
+
+    # Expand expression to do the addition distribution and try to solve each
+    # sub integral
+    expression = alg_expand(expression)
+    int_solution = Integer(0)
+    if type(expression) is Add:
+        for item in expression.args:
+            try:
+                aux_int = tree_solve(item, differential, level+1)
+                int_solution = alg_add(int_solution, aux_int["symbol"])
+                node_difficulty = node_difficulty + aux_int["difficulty"]
+                if node_difficulty >= MAX_NODE_DIFFICULTY:
+                    raise CompletenessAnomaly([["", []]])
+            
+            except CompletenessAnomaly as ca:
+                raise ca
+
+    if int_solution != Integer(0):
+        print("Expression: " + str(expression))
+        print("Level: " + str(level))
+        print("Difficulty: " + str(node_difficulty))
+        print()
+        return {"symbol": int_solution, "difficulty": node_difficulty}
+
+def int_solve(expression, differential):
+    '''
+    # Params
+    * expression (symbolic expression)
+    * differential (symbolic expression)
+
+    # Return (symblic expression)
+    * integrate of the expression with respect the differential. 
+    Uses branch and global difficulty control
+    '''
+
+    try:
+        solution = tree_solve(expression, differential, 0)
+        controller.global_difficulty = controller.global_difficulty + solution["difficulty"]
+        if controller.global_difficulty >= MAX_GLOBAL_DIFFICULTY:
+            raise CompletenessAnomaly([["", []]])
+        return solution["symbol"]
+
+    except CompletenessAnomaly:
+        print("Integral solved with sympy")
+        print()
+        controller.global_difficulty = controller.global_difficulty + MAX_NODE_DIFFICULTY
+
+        if controller.global_difficulty >= MAX_GLOBAL_DIFFICULTY:
+            raise CompletenessAnomaly([["", []]])
+        return integrate(expression, differential)
 
 def match_integral(expression, integral):
     if integral == expression:
         return True
 
-    if integral == Symbol('n'):
-        if type(expression) is Integer:
-            global variable_n
-            if variable_n is None:
-                variable_n = expression
-                return True
-            else:
-                if variable_n == expression:
+    global variables
+    for variable in variables:
+        if integral == variable['symbol']:
+            if expression.is_number:
+                if variable['value'] is None:
+                    variable['value'] = expression
                     return True
                 else:
-                    return False
-        else:
-            return False
-
-    if integral == Symbol('a'):
-        if type(expression) is Integer:
-            global variable_a
-            if variable_a is None:
-                variable_a = expression
-                return True
+                    return variable['value'] == expression
             else:
-                if variable_a == expression:
-                    return True
-                else:
-                    return False
-        else:
-            return False
-
-    if integral == Symbol('b'):
-        if type(expression) is Integer:
-            global variable_b
-            if variable_b is None:
-                variable_b = expression
-                return True
-            else:
-                if variable_b == expression:
-                    return True
-                else:
-                    return False
-        else:
-            return False
-    
-    if integral == Symbol('c'):
-        if type(expression) is Integer:
-            global variable_c
-            if variable_c is None:
-                variable_c = expression
-                return True
-            else:
-                if variable_c == expression:
-                    return True
-                else:
-                    return False
-        else:
-            return False
+                return False
 
     if type(expression) is type(integral):
         item_index = 0
